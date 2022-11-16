ed15e60975d3e3a644c12a37ce62a8d72668f865
Timothé Boone <timotheboone@gmail.com>
2022-11-16 12:13:39 +0100

    arch : Create architecture

---
+++ src/main.c
@@ -0,0 +1,16 @@
+/**
+ * @file main.c
+ * @author Timothe Boone & Inès Guitton
+ * @brief Main program
+ * @version 1.0
+ * @date 16/11/2022
+ * 
+ */
+#include "../include/main.h"
+
+void main() {
+    // Players conections
+    printf("Hello world!");
+
+    
+}
