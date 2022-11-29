8d474fedb07933c96af92ce214c52d25530cac55
Timothé Boone <timotheboone@gmail.com>
2022-11-29 14:38:39 +0100

    main : Ask player name

--- include/main.h
+++ include/main.h
@@ -3,4 +3,28 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define MAX_NUMBER_PLAYER 3
+#define MAX_PLAYER_NAME_LENGTH 64
+
+
+static inline void ErrorInputStringTooLong();
+static inline void ClearInputBuffer();
+
+typedef struct {
+    char name[MAX_PLAYER_NAME_LENGTH];
+    char character;
+} Player;
+
+
+typedef struct {
+    unsigned int numberPlayers;
+    Player playersTab[MAX_NUMBER_PLAYER];
+} Party;
+
 
